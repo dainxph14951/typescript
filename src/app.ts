a1b1166584962aac5941e3b3501bf9ea3f204254
daibede <xuandai300092@gmail.com>
2022-03-12 17:18:11 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,14 @@ const myObject: object = {};
 
 const numberArr: number[] = [1,2,3,4];
 const stringArr: User[] = [{id:1,name:"dai"}];
+
+// 
+type product = {
+    id: number,
+    name: string
+}
+
+const getProducts = <T extends product>(products : T[]) => {
+    const result = products.map(item => `<div>${item.name}</div>`)
+}
+getProducts([{id: 1, name: "a"}, {id: 2, name: "b"}]);
